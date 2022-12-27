bb8a08dee058615e9e53899bdead0f9217e6bde2
t08mac23 <t08mac23@gmail.com>
2022-12-27 13:57:36 +0900

    アクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,20 @@ class Item < ApplicationRecord
 
   validates :item_name,             presence: true
   validates :description,           presence: true
-  validates :category_id,           presence: true
-  validates :condition_id,          presence: true
-  validates :delivery_charge_id,    presence: true
-  validates :prefecture_id,         presence: true
-  validates :delivery_date_id,      presence: true
+  validates :category_id,           numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id,          numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_charge_id,    numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id,         numericality: { other_than: 0 , message: "can't be blank" }
+  validates :delivery_date_id,      numericality: { other_than: 1 , message: "can't be blank" }
   validates :price,                 presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_date
 end
