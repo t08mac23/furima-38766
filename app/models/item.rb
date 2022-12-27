f8d9bae2541ef46f033179ad06e872ccc4840596
t08mac23 <t08mac23@gmail.com>
2022-12-27 16:42:52 +0900

    createアクションとprice
    出品してDBに保存できる

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,11 @@ class Item < ApplicationRecord
   validates :delivery_charge_id,    numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id,         numericality: { other_than: 0 , message: "can't be blank" }
   validates :delivery_date_id,      numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price,                 presence: true
+  
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
+  end
 
   belongs_to :user
   has_one_attached :image
