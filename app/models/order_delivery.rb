373b9d2f3c9875f831ead9ec93cad4ee1f24b1b0
t08mac23 <t08mac23@gmail.com>
2023-01-06 15:03:59 +0900

    他機能関連
    １：売却済みの商品は、画像上に「sold out」の文字が表示されるようになっていること（商品一覧機能）。
    
     ２：売却済みの商品は、画像上に「sold out」の文字が表示されるようになっていること（商品詳細機能）。
    
     ３：ログイン状態の場合でも、売却済みの商品には、「商品の編集」「削除」「購入画面に進む」ボタンが表示されないこと（商品詳細機能）。
    
     ４：ログイン状態の場合でも、自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移すること（商品情報編集機能）。

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -4,7 +4,7 @@ class OrderDelivery
 
   with_options presence: true do
     validates :user_id, :address1, :address2
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Input only number"}
   end
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank" }
