3ac1d06e3597fede53af3225bcf817d19d915366
t08mac23 <t08mac23@gmail.com>
2023-01-14 16:09:07 +0900

    masterではなくmainに！

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'furima-38766'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:t08mac23/furima-38766.git'
-
+set :branch, 'main'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
